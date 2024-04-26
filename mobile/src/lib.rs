8894d75f5485df180272cf1f7687c4970b719841
ShenMian <sms_school@outlook.com>
2024-04-26 15:47:17 +0800

    feat: initial commit

---
+++ mobile/src/lib.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+use bevy::window::WindowMode;
+
+#[bevy_main]
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resizable: false,
+                mode: WindowMode::BorderlessFullscreen,
+                ..default()
+            }),
+            ..default()
+        }),))
+        .run()
+}
