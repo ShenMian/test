2faf7609bc61332ca503a44ab8f990545f656d5d
ShenMian <sms_school@outlook.com>
2024-05-20 15:22:25 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,18 @@ use bevy::prelude::*;
 use soukoban_rs::SokobanPlugin;
 
 fn main() {
-    App::new().add_plugins(SokobanPlugin).run();
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Sokoban".to_string(),
+                    canvas: Some("#bevy".to_owned()),
+                    prevent_default_event_handling: false,
+                    ..default()
+                }),
+                ..default()
+            }),
+            SokobanPlugin,
+        ))
+        .run();
 }
