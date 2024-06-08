4e413372b2f8dd02873c0cc53b9b5d1ab4fc36ce
ShenMian <sms_school@outlook.com>
2024-06-08 13:59:25 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
                     title: "Sokoban".to_string(),
-                    canvas: Some("#bevy".to_owned()),
+                    canvas: Some("#bevy".to_string()),
                     prevent_default_event_handling: false,
                     ..default()
                 }),
                 ..default()
             }),
-            // SokobanPlugin,
+            SokobanPlugin,
         ))
         .run();
 }
